7a3a59560273fec2546764d10a53b58d78b4bb65
wowvain-dev <wowvain.dev@gmail.com>
2021-02-19 12:49:52 +0200

    Am dat putin clean-up la clase.

--- app/src/main/java/com/application/schoolsoft/LoginPage.kt
+++ app/src/main/java/com/application/schoolsoft/LoginPage.kt
@@ -1,7 +1,6 @@
 package com.application.schoolsoft
 
 import android.content.Intent
-import android.nfc.Tag
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -10,7 +9,6 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
