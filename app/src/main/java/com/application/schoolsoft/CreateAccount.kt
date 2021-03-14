93988e4f9eae35f74aea4a88e84afc2d723165d2
Bogdan Stanciu <inquiries@wowvain.tech>
2021-03-14 19:42:52 +0200

    Am adaugat câmp pentru username în activiatea de creare a contului și am făcut să se adauge la baza de date usernameul contului odată cu crearea contului.

--- app/src/main/java/com/application/schoolsoft/CreateAccount.kt
+++ app/src/main/java/com/application/schoolsoft/CreateAccount.kt
@@ -8,14 +8,15 @@ import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class CreateAccount : AppCompatActivity(), View.OnClickListener {
     private lateinit var auth: FirebaseAuth
     private lateinit var createButton: Button
     private lateinit var backButton: ImageView
-    private lateinit var name: String
-    private lateinit var email: String
+    private val KEY_USER: String = "Username"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_account)
@@ -37,24 +38,19 @@ class CreateAccount : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v?.id) { R.id.createAcc2 -> {
             createAccount()
-            val user = auth.currentUser
-            if ( user != null ) {
-                Toast.makeText(baseContext, "Contul a fost creat, logați-vă",
-                        Toast.LENGTH_LONG).show()
-                openAlegeClasa()
-            }
-
         }}
         when (v?.id) {R.id.backButton2 -> {
-            openLogin()
+            finish()
         }}
     }
 
     private fun createAccount() {
         val emailTxt = findViewById<View>(R.id.createEmail) as EditText
+        val createUser = findViewById<View>(R.id.createUser) as EditText
         val passwordTxt = findViewById<View>(R.id.createPassword) as EditText
         val confirmPass = findViewById<View>(R.id.confirmPassword) as EditText
         var email = emailTxt.text.toString()
+        var username = createUser.text.toString()
         var password = passwordTxt.text.toString()
         var conPass = confirmPass.text.toString()
         if (conPass == password) {
@@ -62,9 +58,13 @@ class CreateAccount : AppCompatActivity(), View.OnClickListener {
                     .addOnCompleteListener(this) { task ->
                         if (task.isSuccessful) {
                             Log.d("createAcc", "createUserWithEmail:success")
+                            Toast.makeText(baseContext, "Contul a fost creat",
+                                    Toast.LENGTH_SHORT).show()
+                            addUserDb(username)
+                            openAlegeClasa()
                         } else {
                             Log.w("createAcc", "createUserWithEmail:failure", task.exception)
-                            Toast.makeText(baseContext, "Authentication failed",
+                            Toast.makeText(baseContext, "Autentificare eșuată",
                                     Toast.LENGTH_SHORT).show()
                         }
                     }
@@ -74,16 +74,18 @@ class CreateAccount : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    private fun openLogin() {
-        finish()
-        /*val intent = Intent(this, LoginPage::class.java)
-        startActivity(intent)*/
-    }
-
     private fun openAlegeClasa() {
         val intent = Intent(this, AlegeClasa::class.java)
         startActivity(intent)
         finish()
     }
 
+    private fun addUserDb(username: String) {
+        val userID: String = auth.uid.toString();
+        val database = Firebase.firestore
+        val data = hashMapOf(KEY_USER to username)
+        database.collection("Users").document(userID).set(data, SetOptions.merge()).
+        addOnSuccessListener { Log.d("db.user", "Username successfully set!") }.
+        addOnFailureListener { Log.w("db.user", "Set Username Failure")}
+    }
 }
