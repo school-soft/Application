851c37a6718ee540e5c4c74fbbc9e500d384cdd3
bogdangosa <57357871+bogdangosa@users.noreply.github.com>
2021-03-14 14:08:38 +0200

    am creat butonul de delete account

--- app/src/main/java/com/application/schoolsoft/SettingsActivity.java
+++ app/src/main/java/com/application/schoolsoft/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.application.schoolsoft;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,7 +11,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.ktx.Firebase;
 
 public class SettingsActivity extends ActivityBase {
 
@@ -42,16 +47,46 @@ public class SettingsActivity extends ActivityBase {
             }
         });
     }
-    
+
     private void CreateDeleteAccountButton(){
         TextView DeleteAccountBtn = findViewById(R.id.delete_account_btn);
         DeleteAccountBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(SettingsActivity.this, "You Clicked Delete account", Toast.LENGTH_SHORT).show();
+                DeleteAccount();
+                OpenLogin();
             }
         });
     }
+    private void DeleteAccount(){
+        FirebaseUser user = mAuth.getCurrentUser();
+        String UserID = mAuth.getUid();
+        fStore.collection("Users").document(UserID).delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(SettingsActivity.this, "document deleted succesfuly", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(SettingsActivity.this, "Error occured while deleting the document", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(SettingsActivity.this, "Account deleted succesfuly", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(SettingsActivity.this, "Error occured while deleting the account", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+
     private void OpenLogin(){
         Intent intent = new Intent(this, LoginPage.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
